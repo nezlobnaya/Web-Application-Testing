ff8f92e95a32cc0ec87226ee8c6f88880c259d5c
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-10-10 12:55:07 -0400

    Installed rtl and jest-dom and wrote the first test

--- test-react-app/src/App.test.js
+++ test-react-app/src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+afterEach(rtl.cleanup);//a clean up function will run after ech test runs
+
+test ('renders without crashing',() => {
+  const wrapper = rtl.render(<App />) //passing a component to render
+  wrapper.debug() // let us look into component how JSX is structured
+})
+
